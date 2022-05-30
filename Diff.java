0f6f9fdd5a883878cb438d5461a1b13ff867917f
Vishesh Singhal <visheshsinghal@gmail.com>
2022-05-30 14:01:45 +0530

    Inserted Diff java

---
+++ Diff.java
@@ -0,0 +1,9 @@
+
+public class Diff {
+
+    public public static void main(String[] args) {
+        int a= 23;
+        int b= 45;
+        System.out.println("The difference of a and b is :"+ a-b );
+    }
+}
