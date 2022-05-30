5a9f7d68b34dad6ef8dc41d76554657d3ea9af77
Vishesh Singhal <visheshsinghal@gmail.com>
2022-05-30 14:50:40 +0530

    Changes in Diff.java

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@
 public class Diff {
 
     public public static void main(String[] args) {
-        int a= 23;
+        int a= 28;
         int b= 45;
         System.out.println("The difference of a and b is :"+ a-b );
     }
