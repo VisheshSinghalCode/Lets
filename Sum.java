ba4b3985e6379e0c89ea9ccf5eea7bd7a887679d
Vishesh Singhal <visheshsinghal@gmail.com>
2022-05-30 14:07:56 +0530

    Everything

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public class Sum {
     public public static void main(String[] args) {
         int a= 23;
         int b= 45;
-        System.out.println("The sum is :"+ a+b );
+        System.out.println("The sum of a and b is :"+ a+b );
     }
 }
