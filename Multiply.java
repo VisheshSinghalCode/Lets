7274d294fe31241165866cf35fae772f5d64700d
Vishesh Singhal <visheshsinghal@gmail.com>
2022-05-30 14:26:49 +0530

    Multiply Commit

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+
+public class Multiply {
+
+    public public static void main(String[] args) {
+        int a= 23;
+        int b= 45;
+        System.out.println("The multiplication of a and b is :"+ a*b );
+    }
+}
